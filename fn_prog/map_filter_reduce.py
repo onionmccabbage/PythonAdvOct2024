582578cf356557e4b31d88433a94130040bdb9d0
onionmccabbage <onionmccabbage@live.com>
2024-10-07 16:06:33 +0100

    using map, filter and reduce

--- fn_prog/map_filter_reduce.py
+++ fn_prog/map_filter_reduce.py
@@ -1,4 +1,6 @@
 import util # we now have access to all the utility functions
+# reduce is part of the functools library
+import functools
 
 def isOdd(w):
     return w%2 != 0 # we are assuming these are integers
@@ -15,8 +17,20 @@ def main():
         print(_)
 
     # in a similar way, we can use 'filter' to return members that match a criteria
-    odd_nums = filter(isOdd, range(0,100))
-    print(odd_nums) # .....?
+    odd_nums = filter(isOdd, range(0,100)) # NB we need True or False
+    # we can use 'reduce' to iteratively apply a function to return a cumulative single result
+    print(odd_nums) # we have a filter object
+    print( odd_nums.__next__() ) # 1
+    print( odd_nums.__next__() ) # 3
+    print( odd_nums.__next__() ) # 5
+    print( odd_nums.__next__() ) # 7
+    print('----------')
+    for _ in odd_nums:
+        print(_, end=', ')
+
+    # there are no nore values in the filter object, so we cannot see any more results
+    total = functools.reduce( util.addThem, range(1,11) ) # 55
+    print(total)
 
 if __name__ == '__main__':
     main()
