922a488941d7253c5332f361b77655a171e96980
onionmccabbage <onionmccabbage@live.com>
2024-10-07 11:45:00 +0100

    sample shape with several properties

--- some_classes/my_shape.py
+++ some_classes/my_shape.py
@@ -8,12 +8,13 @@ class Shape(AbstractShape): # we choose to inherit from our ABC
     This class captures the number of sides, size and colour of a shape'''
     # delcare slots for the mangled properties of this class
     __slots__ = ('__num_sides', '__size', '__colour' ) # a tuple of permitted name-mangled members
-    def __init__(self, num_sides) -> None: # here is an annotation reminding us this method will return nothing
+    def __init__(self, num_sides, size, colour) -> None: # here is an annotation reminding us this method will return nothing
         '''the __init__ method is similar to a constructor
         it will run once every time we make an instance of this class'''
         # this will call our num_sides setter method
         self.num_sides = num_sides # we set initial properties in the __init__ method
-        self.__coffee = 'broken'
+        self.size = size
+        self.colour = colour
     @property # this is a decorator (begins with @)
     def num_sides(self):
         '''return the name-mangled property (a bit like private properties)'''
@@ -40,7 +41,7 @@ class Shape(AbstractShape): # we choose to inherit from our ABC
     @colour.setter
     def colour(self, colour):
         if type(colour)==str and colour !='':
-            self.__colour == colour
+            self.__colour = colour
         else:
             raise TypeError('Colour must be a non-empty string')
     #our abstract base class insists that we implement a __str__ method
@@ -51,12 +52,14 @@ def main():
     '''call this function'''
     '''make instances of our class'''
     try:
-        s1 = Shape(3) # this will invoke the __init__ method once
+        # shape needs num_sides, size and colour
+        s1 = Shape(3, 4.5, 'green') # this will invoke the __init__ method once
     except TypeError as te:
         print(f'Problem: {te}') # return a helpful message
     # can we access mangled members?
-    s1.__num_sides = 'not accessible' # this will throw an error
-    print(s1.__num_sides)
+    # s1.__num_sides = 'not accessible' # this will throw an error
+    s1.__colour = '...'
+    # print(s1.__num_sides)
     print(s1) # this will invoke the __str__ method of our class
 
 
