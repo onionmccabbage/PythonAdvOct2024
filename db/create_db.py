46cba53aa3c064e1e12cdde7a5de8834a2741668
onionmccabbage <onionmccabbage@live.com>
2024-10-08 11:19:49 +0100

    simple database access

--- db/create_db.py
+++ db/create_db.py
@@ -4,14 +4,14 @@ def accessDB():
     '''create a database then add a table to it'''
     conn = sqlite3.connect('my_db') # either create or else open this database
     curs = conn.cursor() # this is the database access mechanism
-    # we need an SQL statement
+    # we need an SQL statement (note the round brackets)
     st = '''
     CREATE TABLE zoo
-    {
+    (
         creature VARCHAR(32) PRIMARY KEY,
         count int,
         cost float
-    }
+    )
 '''
     try:
         curs.execute(st)
@@ -19,3 +19,6 @@ def accessDB():
         conn.close() # tidy up
     except Exception as e:
         print(e)
+
+if __name__ == '__main__':
+    accessDB() # run this ONCE to create DB and a table
