3f661561a7615cab7f6699aee74a6a9d9498bbaa
onionmccabbage <onionmccabbage@live.com>
2024-10-08 10:29:06 +0100

    accessing external API data

--- text_json/using_requests.py
+++ text_json/using_requests.py
@@ -5,9 +5,12 @@ def getData():
     '''make a request to an API and expect to receive JSON'''
     apiURL = 'https://jsonplaceholder.typicode.com/photos'
     response = requests.get(apiURL) # we receive a response object, which includes any data
+    # we can access many parts of the response object
+    print(response.status_code, response.raw)
     res_j = response.json() # this will take the JSON and convert to a Python structure
-    return res_j
+    return res_j # we have a list of dictionaries
 
 if __name__ == '__main__':
     r = getData()
-    print(r, type(r))
+    # print(r, type(r))
+    print(r[42], type(r[42]))
