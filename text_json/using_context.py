4e45a89a1593f9bf373e12a55c28ca0387c7d3cd
onionmccabbage <onionmccabbage@live.com>
2024-10-08 15:43:03 +0100

    using context manager

--- text_json/using_context.py
+++ text_json/using_context.py
@@ -13,7 +13,7 @@ if __name__ == '__main__':
     # first check the existing sys.stdout
     sys.stdout.write('This stream is output by the default sys.stdout') # same as calling print()
     with open('my_stream.txt', 'a') as fobj: # we now have a file access object
-        with outputRedirect(obj):
+        with outputRedirect(fobj):
             print('This will be written to our file')
             sys.stdout.write('...more file output')
     print('back to the normal output')
