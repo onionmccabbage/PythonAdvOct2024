e737ab64e82ec0f1e34b07743afeb90a54f1376c
onionmccabbage <onionmccabbage@live.com>
2024-10-09 16:18:28 +0100

    using unittest

---
+++ unit_tests/point_test.py
@@ -0,0 +1,22 @@
+import unittest
+from point import Point
+
+class testPoint(unittest.TestCase):
+    '''here we test the Point class'''
+    def setUp(self):
+        '''this method will be executed before the tests are run'''
+        self.point = Point(3,5) # a sensible instance for use in each test
+
+    # declare some independent tests (setUp will run before each)
+    def testMoveBy(self):
+        self.point.moveBy(-5, -2)
+        self.assertEqual(self.point.display(), (-2, 3) )
+    def testMoveByXY(self):
+        self.point.moveBy(5,2)
+        self.assertEqual( self.point.x, 8 )
+        self.assertEqual( self.point.y, 7 )
+    def testRaises(self):
+        self.assertRaises( self.point.moveBy('a', 'b'), TypeError )
+
+if __name__ == '__main__':
+    unittest.main() # invoke the tests
