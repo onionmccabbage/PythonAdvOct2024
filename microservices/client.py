57f37beb5ae4b355367151ed87619f448b4bde7b
onionmccabbage <onionmccabbage@live.com>
2024-10-09 10:42:05 +0100

    client with sys.argv

--- microservices/client.py
+++ microservices/client.py
@@ -1,11 +1,17 @@
 import socket
+import sys
 
 def client():
     '''a simple microservices client'''
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     port_t = ('localhost', 9874)
     client.connect(port_t) # we atempt to connect to our microservice server
-    client.send(b'hello') # we must encode as bytes
+    # if there are additional system argument variables, send them to the server
+    if len(sys.argv) > 1: # we have arguments!
+        message = ' '.join(sys.argv[1:]) # slice the sys.argv to leave off the zeroth member
+    else:
+        message = 'default'
+    client.send(message.encode()) # we must encode as bytes
     # see if there is a response from the server
     data = client.recv(1024)
     print(f'Cleint received {data}')
