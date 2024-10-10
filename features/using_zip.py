6ddbeae7583f148ed3c77929efb4d63a33f304ae
onionmccabbage <onionmccabbage@live.com>
2024-10-10 15:35:43 +0100

    using zip

--- features/using_zip.py
+++ features/using_zip.py
@@ -3,11 +3,14 @@
 def main():
     '''We use zip to combine unrelatetd structures into a single structure '''
     days_l = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri']
-    fruits_t = ('Banana', 'Orange', 'Kiwi')
+    fruits_t = ('Banana', 'Orange', 'Kiwi', 'Durian')
+    drinks_s = {'Coffee', 'Tea', 'Water', 'Calpico'}
 
     # zip lets us cmbine any iterable structures together
-    z = zip(days_l, fruits_t)
+    z = zip(days_l, fruits_t, drinks_s)
     print( z, type(z) )
+    for day, fruit, drink in z:
+        print(f'On {day} I ate {fruit} with {drink}')
 
 if __name__ == '__main__':
     main()
