d7c4fe61f193ecccebf4a4505e0e0cecae1df389
onionmccabbage <onionmccabbage@live.com>
2024-10-10 10:03:54 +0100

    simple threaded functions

---
+++ multithreading/01_threads.py
@@ -0,0 +1,28 @@
+from threading import Thread
+import time # we will sleep to represent long-running functionality
+
+# any function may be invoked on a separate thread
+def myFn(n):
+    '''this is a normal function, just like any other'''
+    time.sleep( 1 ) # sleep for one second
+    print(f'this is the function: {n}')
+
+if __name__ == '__main__':
+    # we may invoke the function in the normal way (procedurally)
+    # myFn(1)
+    # myFn(5)
+    # myFn(3)
+    # myFn(2)
+    # myFn(4)
+    tA = Thread(target=myFn,  args=('A',)) # a one-member tuple
+    tB = Thread(target=myFn,  args=('B',)) 
+    tC = Thread(target=myFn,  args=('C',)) 
+    tD = Thread(target=myFn,  args=('D',)) 
+    tE = Thread(target=myFn,  args=('E',)) 
+    print('on the main thread')
+    tA.start() # at this point the function will start execution on a separate thread
+    tB.start()
+    tC.start()
+    tD.start()
+    tE.start()
+    print('still on the main thread')
