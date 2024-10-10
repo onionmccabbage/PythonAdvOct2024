52adc579a1a760d96fd4a844451e000ec7bfa110
onionmccabbage <onionmccabbage@live.com>
2024-10-10 10:20:56 +0100

    using threads with classes

---
+++ multithreading/02_thread_class.py
@@ -0,0 +1,33 @@
+from threading import Thread
+import time
+import random # we will sleep for random times
+
+class MyClass(Thread): # we inherit from the Thread class
+    '''Any class may inherit from Thread'''
+    def __init__(self, n, x):
+        # call the __init__ of the parent class (Thread)
+        Thread.__init__(self)
+        self.n = n # we could use @property setter/getter functions
+        self.x = x
+    # we override the run method of Thread
+    def run(self):
+        '''When this class is invoked, the run method will execute it on a new thread'''
+        for _ in range(0,10): # on average, this class will run for 0.5 sec
+            print(f'{self.n} is sleeping')
+            time.sleep(random.random()*0.1) # sleep for up to 0.1 second
+
+if __name__ == '__main__':
+    print('On the main thread')
+    tA = MyClass('A', True)
+    tB = MyClass('B', [])
+    tC = MyClass('C', (3,2,1))
+    tD = MyClass('D', 'data')
+    tE = MyClass('E', {'A', True, 44, None})
+    print('Starting the thread classes')
+    tA.start()
+    tB.start()
+    tC.start()
+    tD.start()
+    tE.start()
+
+    print('where is this printed.....')
