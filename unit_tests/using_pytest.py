949ca0253d6234e79d1c3bcf4742d7092cf4ab0a
onionmccabbage <onionmccabbage@live.com>
2024-10-10 09:44:16 +0100

    using pytest with named tuples

--- unit_tests/using_pytest.py
+++ unit_tests/using_pytest.py
@@ -3,8 +3,17 @@ from collections import namedtuple
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
 Task.__new__.__defaults__ = (None, None, False, None)
 
+# invoke pyest like this:
+# python -m pytest -v using_pytest.py
+
+def testDefaults():
+    '''using no parameters should invoke the defaults'''
+    tA = Task() # use all the defaults
+    tC = Task(None, None, False, None)
+    assert tA == tC # this is a pyset assertion
+    # tB = Task('Learn testing', 'Deidre', False, 1)
+
 if __name__ == '__main__':
     '''use our named tuple'''
-    tA = Task() # use all the defaults
-    tB = Task('Learn testing', 'Deidre', False, 1)
-    print(tA, tB, type(tA))
+    
+    # print(tA, tB, type(tA))
